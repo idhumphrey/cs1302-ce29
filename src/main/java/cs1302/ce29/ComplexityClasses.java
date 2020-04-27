fe0b3d4a68f100e9e7e39c612f457d4827fa6a16
Bella <idh48440@uga.edu>
2020-04-26 21:24:13 -0400

    checkpoint-1

--- src/main/java/cs1302/ce29/ComplexityClasses.java
+++ src/main/java/cs1302/ce29/ComplexityClasses.java
@@ -9,7 +9,7 @@ import java.util.stream.IntStream;
 import java.util.Arrays;
 
 /**
- * A JavaFX application containing a {@code LineChart} with series for each of the following
+ * A JavaFX application containing a {@code LineChart} with series for each of the following.
  * complexity classes: <br>
  *  - Constant Time <br>
  *  - Logarithmic Time <br>
@@ -37,13 +37,22 @@ public class ComplexityClasses extends Application {
 
     /**
      * The entry point for the JavaFX application
+     *
+     * @param Stage the stage of the application.
      */
     public void start(Stage stage) {
 
         // Initialize the values for the x-axis (Problem Size)
-	Integer[] x = IntStream.range(X_START, X_FINAL)
-	    .mapToObj(i -> i)
-	    .toArray(Integer[]::new);
+        Integer[] x = IntStream.range(X_START, X_FINAL)
+            .mapToObj(i -> i)
+            .toArray(Integer[]::new);
+
+        //Sample from genData JavaDoc
+
+        Double[] y = genData(x, n -> n + 10.0);
+
+        Arrays.stream(y)
+            .forEach(System.out::println);
 
         // Create initial line chart and add the constant time data series
         lc = ChartUtility.createChart(x, genData(x, n -> 10.0), "Size", "Operations", Y_FINAL, "Constant");
@@ -61,16 +70,16 @@ public class ComplexityClasses extends Application {
     } // start
 
     /**
-     * Creates and populates an array of {@code Double} objects using an array of 
-     * {@code Integer} objects as input. Each value in the created array is 
-     * an element of the {@code Integer} array mapped to a {@code Double} via the 
+     * Creates and populates an array of {@code Double} objects using an array of
+     * {@code Integer} objects as input. Each value in the created array is
+     * an element of the {@code Integer} array mapped to a {@code Double} via the
      * specified {@code Function} object. The elements should be mapped in order.
-     * For example, consider generating an array for the function 
+     * For example, consider generating an array for the function
      * <pre>{@code f(n) = n + 10}</pre>
      * using a call to
      * <pre>{@code genData(x, n -> n + 10.0);}</pre>
-     * where {@code x = [0, 1]}. This  would return {@code [10.0, 11.0]}. Each value in 
-     * the new array is each element in {@code x} plus {@code 10.0}, as defined by 
+     * where {@code x = [0, 1]}. This  would return {@code [10.0, 11.0]}. Each value in
+     * the new array is each element in {@code x} plus {@code 10.0}, as defined by
      * {@code f}. The new array should be the same length as {@code x}.
      *
      * @return the double array containing the mapped values
@@ -78,8 +87,13 @@ public class ComplexityClasses extends Application {
      * @param f the function to map index values to doubles
      */
     public Double[] genData(Integer[] x, Function<Integer, Double> f) {
-        // TODO: Implement Me!
-        throw new UnsupportedOperationException("genData method not yet implemented");
+
+        Double[] newArray  = Arrays.stream(x)
+            .map(e -> f.apply(e))
+            .toArray(Double[]::new);
+
+        return newArray;
+        //throw new UnsupportedOperationException("genData method not yet implemented");
     } // genData
 
 } // ComplexityClasses
